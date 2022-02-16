cd40bc6f81430d68f9a2dbd8be8e624786da7f52
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-02-16 13:28:04 -0500

    update testers

--- src/index.js
+++ src/index.js
@@ -594,7 +594,7 @@ app.get('/api/v1/formations-teams/:wallet',async(req,res) => {
         }
     }
 
-    res.send("1,"+formaciones.toString()+","+inventario.toString());
+    res.send(inventario.toString()+"1,"+formaciones.toString());
 });
 
 app.get('/api/v1/coins/:wallet',async(req,res) => {
