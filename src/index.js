5ed23bb17e9de832769f860a3281946c1c8f02fe
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-02-21 12:21:59 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1387,7 +1387,7 @@ app.get('/api/v1/misionesdiarias/tiempo/:wallet',async(req,res) => {
 });
 
 async function resetChecpoint(wallet){
-    var usuario = await user.find({ wallet: uc.upperCase(wallet) },{_id:0});
+    var usuario = await user.find({ wallet: uc.upperCase(wallet) },{_id:0, __v:0});
     usuario = usuario[0];
     var datos = usuario
 
