f622cff8a8369fc03065b28303fac3a83e7e3113
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-03-14 15:36:44 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2042,7 +2042,7 @@ app.get('/api/v1/app/apuestas/',async(req,res) => {
 
 app.get('/api/v1/consulta/miranking/:wallet',async(req,res) => {
 
-    var wallet =  req.params.wallet;
+    var wallet =  uc.upperCase(req.params.wallet);
 
     var myuser = await playerData.findOne({wallet: wallet},
         {_id:0,BallonSet:0,DificultConfig:0,LastDate:0,PlaysOnlineTotal:0,LeaguesOnlineWins:0,DiscountMomment:0,DuelsOnlineWins:0,DuelsPlays:0,FriendLyWins:0,FriendlyTiming:0,LeagueDate:0,LeagueOpport:0,LeagueTimer:0,MatchLose:0,MatchWins:0,MatchesOnlineWins:0,Music:0,PhotonDisconnected:0,QualityConfig:0,StadiumSet:0,PlaysTotal:0,TournamentsPlays:0,Version:0,VolumeConfig:0,Plataforma:0,GolesEnContra:0,GolesAFavor:0,FirstTime:0,DrawMatchs:0,DrawMatchsOnline:0,LeaguePlay:0,Analiticas:0,Fxs:0,__v:0,Soporte:0,Fullscreen:0,Resolucion:0}
