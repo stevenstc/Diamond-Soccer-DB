598c60172b375215a9c239ddd225825615bebc1b
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-07-19 23:07:28 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1199,11 +1199,12 @@ app.post('/api/v1/consulta/dailymission/:wallet',async(req,res) => {
     if(web3.utils.isAddress(wallet)){
 
         var data = await playerData.find({wallet: uc.upperCase(wallet)});
+        var user = await user.findOne.find({wallet: uc.upperCase(wallet)})
 
         if (data.length >= 1) {
             data = data[0];
         
-            res.send(data.TournamentsPlays+","+data.DuelsPlays+","+data.FriendLyWins+","+data.reclamado);
+            res.send(data.TournamentsPlays+","+data.DuelsPlays+","+data.FriendLyWins+","+user.reclamado);
 
         }else{
 
